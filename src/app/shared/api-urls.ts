8c7d2b194d4b70ed93c8db8f865757f79fe570fa
Zhuan <jowan@abv.bg>
2022-07-11 23:41:16 +0300

    api urls todos and comments

--- src/app/shared/api-urls.ts
+++ src/app/shared/api-urls.ts
@@ -19,9 +19,25 @@ export const apiUrls = {
   schoolsUrl: environment.apiBaseUrl + 'api/school',
   getSchoolsUrl: environment.apiBaseUrl + 'api/school/all',
   // Grades
-  gradesUrl: environment.apiBaseUrl + 'api/grade',
+  gradesUrl: environment.apiBaseUrl + 'api/grade', // ROOT
+  getAllGradesUrl: environment.apiBaseUrl + 'api/grade/all', // (GET)
+  getStudentGradesUrl: environment.apiBaseUrl + 'api/grade', // /{studentId} (GET)
+  getParentGradesUrl: environment.apiBaseUrl + 'api/grade/students/parent', // /{parentId} (GET)
+  getTeacherGradesUrl: environment.apiBaseUrl + 'api/grade/students/teacher', // /{teacherId} (GET)
+  getPrincipalGradesUrl: environment.apiBaseUrl + 'api/grade/school', // /{schoolId} (GET)
+  createGradeUrl: environment.apiBaseUrl + 'api/grade', // /{studentId} (POST)
+  updateGradeUrl: environment.apiBaseUrl + 'api/grade', // /{gradeId} (PATCH)
+  deleteGradeUrl: environment.apiBaseUrl + 'api/grade', // /{gradeId} (DELETE)
   // Absences
-  absencesUrl: environment.apiBaseUrl + 'api/absence',
+  absencesUrl: environment.apiBaseUrl + 'api/absence', // ROOT
+  getAllAbsencesUrl: environment.apiBaseUrl + 'api/absence/all', // (GET)
+  getStudentAbsencesUrl: environment.apiBaseUrl + 'api/absence', // /{studentId} (GET)
+  getParentAbsencesUrl: environment.apiBaseUrl + 'api/absence/students/parent', // /{parentId} (GET)
+  getTeacherAbsencesUrl: environment.apiBaseUrl + 'api/absence/students/teacher', // /{teacherId} (GET)
+  getPrincipalAbsencesUrl: environment.apiBaseUrl + 'api/absence/school', // /{schoolId} (GET)
+  createAbsenceUrl: environment.apiBaseUrl + 'api/absence', // /{studentId} (POST)
+  updateAbsenceUrl: environment.apiBaseUrl + 'api/absence', // /{absenceId} (PATCH)
+  deleteAbsenceUrl: environment.apiBaseUrl + 'api/absence', // /{absenceId} (DELETE)
   // Subjects
   subjectsUrl: environment.apiBaseUrl + 'api/subject',
   // TODO Statistics
